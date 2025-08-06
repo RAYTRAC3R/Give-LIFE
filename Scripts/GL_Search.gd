47ea74308179ad062432baef78f86b4528a8749d
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2025-08-06 01:47:24 -0600

    yes

--- Scripts/GL_Search.gd
+++ Scripts/GL_Search.gd
@@ -1,63 +1,66 @@
 extends Control
 
-var rows : Dictionary = {
-	"Add":1,
-	"Advanced Spotlight":1,
-	"Advanced Stage Lights":1,
-	"Animatronic":1,
-	"Audio":1,
-	"Blacklight":1,
-	"Bool":1,
-	"Color":1,
-	"Float":1,
-	"Invert":1,
-	"Keystrokes":1,
-	"Keystroke Ramp":1,
-	"Lerp":1,
-	"Mix Colors":1,
-	"Mix Floats":1,
-	"Mouse Wheel":1,
-	"Multiply":1,
-	"Random":1,
-	"Record":1,
-	"Sawtooth":1,
-	"Sine":1,
-	"Speaker":1,
-	"Subtract":1,
-	"Switch Audio":1,
-	"Timeline":1,
-	}
-var searching : bool
+var nodePaths: Dictionary = {}  # name (from filename) -> full scene path
+var searching: bool
 
 func _ready():
 	_set_State(false)
+	_scan_mod_nodes()
 	_set_rows()
 
 func toggleSearch():
 	_set_State(!searching)
 
-func _set_State(state:bool):
+func _set_State(state: bool):
 	searching = state
 	visible = searching
 
+func _scan_mod_nodes():
+	nodePaths.clear()
+	var mods_dir = DirAccess.open("res://Mods")
+	if not mods_dir:
+		push_error("Mods folder not found.")
+		return
+
+	mods_dir.list_dir_begin()
+	var mod_name = mods_dir.get_next()
+	while mod_name != "":
+		if mods_dir.current_is_dir() and mod_name != "." and mod_name != "..":
+			var node_path = "res://Mods/%s/Mod Directory/Nodes" % mod_name
+			if DirAccess.dir_exists_absolute(node_path):
+				var nodes_dir = DirAccess.open(node_path)
+				nodes_dir.list_dir_begin()
+				var file_name = nodes_dir.get_next()
+				while file_name != "":
+					if file_name.ends_with(".tscn"):
+						var name = file_name.get_basename()  # Strip extension
+						var full_path = "%s/%s" % [node_path, file_name]
+						nodePaths[name] = full_path
+					file_name = nodes_dir.get_next()
+		mod_name = mods_dir.get_next()
+
 func _set_rows():
-	var container = get_node("Panel").get_node("ScrollContainer").get_node("Container")
+	var container = get_node("Panel/ScrollContainer/Container")
 	for child in container.get_children():
 		child.queue_free()
-	for key in rows:
+
+	for name in nodePaths.keys():
 		var row = load("res://Scenes/UI/Search Row.tscn").instantiate()
-		var button = (row.get_node("Button") as Button)
-		button.text = str(key)
+		var button = row.get_node("Button") as Button
+		button.text = name
 		button.pressed.connect(func():
-			_create_node(button.text)
+			_create_node(name)
 		)
 		button.pressed.connect(func():
 			_set_State(false)
 		)
-		container.call_deferred("add_child",row)
+		container.call_deferred("add_child", row)
 
-func _create_node(name:String):
-	var path = "res://Scenes/Node Types/" + name + ".tscn"
+func _create_node(name: String):
+	if not nodePaths.has(name):
+		push_error("Node type not found: " + name)
+		return
+	var path = nodePaths[name]
 	var node = load(path).instantiate()
 	var holder = get_parent().get_node("Holder")
 	holder.add_child(node)
@@ -65,4 +68,3 @@ func _create_node(name:String):
 	node.nodePath = path
 	node.global_position = (get_viewport().size / 2.0) - (node.get_child(0).size / 2.0)
 	node._create_uuid()
-	
