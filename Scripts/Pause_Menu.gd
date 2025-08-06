bfa3a339208bfe708c220ec31366e8581d7d1dae
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2025-08-06 01:28:24 -0600

    mod maps

--- Scripts/Pause_Menu.gd
+++ Scripts/Pause_Menu.gd
@@ -14,6 +14,14 @@ var simulatorButton: Button
 
 var titleScreenMenu: VBoxContainer
 var settingsMenu: MarginContainer
+var modMenu: MarginContainer
+var mapMenu: MarginContainer
+
+var mapListContainer: VBoxContainer
+var mapEntryScene: PackedScene
+var availableMaps := {}  # mapName -> scene path
+var currentMapInstance: Node = null
+
 
 var currentSettings := {
 	"window_mode": 0,
@@ -23,6 +31,7 @@ var currentSettings := {
 	"master_volume": 100,
 	"physics_bones": true,
 	"msaa_3d": Viewport.MSAA_2X,
+	"recent_map": "",
 }
 
 const settingsPath := "user://Settings/"
@@ -31,14 +40,15 @@ const settingsFilePath := settingsPath + "user_settings.cfg"
 var editorInstance: Node = null
 var keypressInstance: Node = null
 var nodeMapScene: PackedScene
-var simulatorScene: PackedScene
 var keypresssScene: PackedScene
-var simulatorInstance: Node = null
 
 func _ready():
 	nodeMapScene = load("res://Scenes/UI/Node Map.tscn")
-	simulatorScene = load("res://Scenes/Levels/FDs.tscn")
 	keypresssScene = load("res://Scenes/UI/Key Presses.tscn")
+
+	mapListContainer = get_node("MarginContainer/PanelContainer/Map Menu/Mods/PanelContainer/VBoxContainer/ScrollContainer/Map Holder") 
+	mapEntryScene = load("res://Scenes/UI/Map Box.tscn")
+	load_maps()
 
 	editorInstance = nodeMapScene.instantiate()
 	get_tree().root.add_child.call_deferred(editorInstance)
@@ -49,6 +59,8 @@ func _ready():
 	versionNumber = get_node("MarginContainer/PanelContainer/Version Number")
 	titleScreenMenu = get_node("MarginContainer/PanelContainer/Title Screen")
 	settingsMenu = get_node("MarginContainer/PanelContainer/Settings")
+	modMenu = get_node("MarginContainer/PanelContainer/Mod Menu")
+	mapMenu = get_node("MarginContainer/PanelContainer/Map Menu")
 	usernameLineEdit = get_node("MarginContainer/PanelContainer/Settings/Settings/TabContainer/General/VBoxContainer/Username/LineEdit")
 	showPressedKeysButton = get_node("MarginContainer/PanelContainer/Settings/Settings/TabContainer/General/VBoxContainer/Show Pressed Keys/CheckButton")
 	bootOnStartOptions = get_node("MarginContainer/PanelContainer/Settings/Settings/TabContainer/General/VBoxContainer/Boot on Start/OptionButton")
@@ -65,15 +77,17 @@ func _ready():
 	# Apply boot on start options
 	match currentSettings["boot_on_start"]:
 		1:
-			load_simulator()
+			var map_name = currentSettings.get("recent_map", "")
+			if map_name != "" and availableMaps.has(map_name):
+				load_map_scene(map_name)
+			else:
+				print("No recent map to load or map not found.")
 		2:
 			self.visible = false
 			editorInstance.visible = true
 
-	update_simulator_button_text()
-
 func _unhandled_input(event):	
-	if simulatorInstance == null:	
+	if currentMapInstance == null:
 		if event.is_action_pressed("Pause") or event.is_action_pressed("Editor"):
 			editorInstance.visible = !editorInstance.visible
 			self.visible = !editorInstance.visible
@@ -86,12 +100,18 @@ func _unhandled_input(event):
 func switchMenu(menu:String):
 	titleScreenMenu.visible = false
 	settingsMenu.visible = false
+	mapMenu.visible = false
+	modMenu.visible = false
 	
 	match(menu):
 		"title":
 			titleScreenMenu.visible =  true
 		"settings":
 			settingsMenu.visible =  true
+		"mods":
+			modMenu.visible =  true
+		"maps":
+			mapMenu.visible =  true
 
 func _on_slider_changed(value:float, name:String):
 	currentSettings[name] = value
@@ -161,40 +181,17 @@ func apply_settings():
 	
 	get_tree().call_group("SettingsReceivers", "on_settings_applied", currentSettings)
 
-func load_simulator():
-	if simulatorInstance == null:
-		simulatorInstance = simulatorScene.instantiate()
-		self.visible = false
-		editorInstance.visible = false
-		update_simulator_button_text()
-		get_tree().root.add_child.call_deferred(simulatorInstance)
-		await simulatorInstance.ready
-		update_mouse_mode()
-		apply_settings()
-
-func unload_simulator():
-	if simulatorInstance != null:
-		simulatorInstance.queue_free()
-		simulatorInstance = null
-
-		self.visible = true
-		update_mouse_mode()
-		update_simulator_button_text()
-
-func is_simulator_loaded() -> bool:
-	return simulatorInstance != null
-
 func update_simulator_button_text():
-	if is_simulator_loaded():
-		simulatorButton.text = "Unload Simulator"
+	if currentMapInstance:
+		simulatorButton.text = "Unload Map"
 	else:
-		simulatorButton.text = "Boot Simulator"
+		simulatorButton.text = "Map List"
 
 func _on_simulator_button_pressed():
-	if is_simulator_loaded():
-		unload_simulator()
+	if currentMapInstance:
+		unload_current_map()
 	else:
-		load_simulator()
+		switchMenu("maps")
 
 func toggle_pause_menu():
 	var will_show = not self.visible
@@ -218,3 +215,88 @@ func update_mouse_mode():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func load_maps():
+	availableMaps.clear()
+	for child in mapListContainer.get_children():
+		child.queuefree()
+
+	var mods_dir := DirAccess.open("res://Mods")
+	if mods_dir == null:
+		push_error("Mods directory not found!")
+		return
+
+	mods_dir.list_dir_begin()
+	var mod_name = mods_dir.get_next()
+	while mod_name != "":
+		if mods_dir.current_is_dir() and mod_name != "." and mod_name != "..":
+			var map_dir_path = "res://Mods/%s/Mod Directory/Maps" % mod_name
+			var maps_dir := DirAccess.open(map_dir_path)
+			if maps_dir:
+				maps_dir.list_dir_begin()
+				var map_folder = maps_dir.get_next()
+				while map_folder != "":
+					if maps_dir.current_is_dir() and map_folder != "." and map_folder != "..":
+						var full_map_path = "%s/%s" % [map_dir_path, map_folder]
+						var map_info_path = full_map_path + "/Map Info.cfg"
+						var icon_path = full_map_path + "/Map Icon.png"
+						
+						var config := ConfigFile.new()
+						if config.load(map_info_path) == OK and ResourceLoader.exists(icon_path):
+							var map_name = config.get_value("map", "maptitle", map_folder)
+
+							# Find first .tscn file
+							var inner_dir := DirAccess.open(full_map_path)
+							var scene_path := ""
+							if inner_dir:
+								inner_dir.list_dir_begin()
+								var f = inner_dir.get_next()
+								while f != "":
+									if f.ends_with(".tscn"):
+										scene_path = "%s/%s" % [full_map_path, f]
+										break
+									f = inner_dir.get_next()
+							
+							if scene_path != "":
+								availableMaps[map_name] = scene_path
+
+								# Create and setup prefab
+								var entry = mapEntryScene.instantiate()
+								entry.get_node("PanelContainer/HBoxContainer/Name").text = map_name
+								entry.get_node("PanelContainer/HBoxContainer/PanelContainer/Icon").texture = load(icon_path)
+
+								var load_button = entry.get_node("PanelContainer/Button")
+								load_button.pressed.connect(load_map_scene.bind(map_name))
+
+								mapListContainer.add_child(entry)
+					map_folder = maps_dir.get_next()
+		mod_name = mods_dir.get_next()
+
+func load_map_scene(map_name: String):
+	if availableMaps.has(map_name):
+		var path = availableMaps[map_name]
+		var scene = load(path)
+		if scene:
+			if currentMapInstance:
+				currentMapInstance.queue_free()
+			currentMapInstance = scene.instantiate()
+			get_tree().root.add_child(currentMapInstance)
+
+			self.visible = false
+			editorInstance.visible = false
+			update_mouse_mode()
+			update_simulator_button_text()
+			switchMenu("title")
+			currentSettings["recent_map"] = map_name
+			save_settings()
+			apply_settings()
+	else:
+		push_error("Map not found or not available: " + map_name)
+
+func unload_current_map():
+	if currentMapInstance:
+		currentMapInstance.queue_free()
+		currentMapInstance = null
+		self.visible = true
+		update_mouse_mode()
+		update_simulator_button_text()
