3bacb0434194f8345b481d57e945ce9507cb117f
The 64th Gamer <69170079+The64thGamer@users.noreply.github.com>
2025-08-05 22:11:42 -0600

    moving around

--- Scripts/Node Types/GL_Output.gd
+++
@@ -1,9 +0,0 @@
-extends GL_Node
-class_name GL_Output
-@export var customRows : Dictionary
-
-func _ready():
-	super._ready()
-	_set_title("Output")
-	special_condition = "Animatable"
-	_update_visuals()
